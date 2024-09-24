3e89f15c34a6548254de92afeb91e10ede73e7a7
minkhant285 <bryyan.mk@gmail.com>
2024-09-24 13:57:15 +0700

    duration command linux error fix

--- src/utils/ffmpeg_thumbnail.ts
+++ src/utils/ffmpeg_thumbnail.ts
@@ -7,7 +7,7 @@ const execPromise = promisify(exec);
 async function runFFmpegCommand(command: string): Promise<string | void> {
     try {
         const { stdout, stderr } = await execPromise(command);
-        // console.log('stdout:', stdout);
+        console.log('stdout:', stdout);
         if (stderr) {
             console.error('stderr:', stderr);
         }
@@ -17,17 +17,26 @@ async function runFFmpegCommand(command: string): Promise<string | void> {
     }
 }
 
-export async function getFFMpegVideoDuration(videoUrl: string): Promise<string> {
-    const unixCommand = `ffmpeg -i ${videoUrl} 2>&1 | grep "Duration" | awk '{print "{\"Duration\": \"" $2 "\"}"}'`;
-    const windowCommand = `for /f "tokens=2 delims= " %a in ('ffmpeg -i ${videoUrl} 2^>^&1 ^| findstr "Duration"') do @echo {"Duration": "%a"}`;
-    if (os.type() === 'Windows_NT') {
-        return await runFFmpegCommand(windowCommand) as string;
-    }
-    return await runFFmpegCommand(unixCommand) as string;
-
-}
 
 export async function generateThumbnail(videoUrl: string, timestamp: string = '00:00:05', thumbnailPath: string) {
     const command = `ffmpeg -ss ${timestamp} -i ${videoUrl}  -vframes 1 -an ${thumbnailPath}`;
     await runFFmpegCommand(command);
+}
+
+
+export async function getFFMpegVideoDuration(videoUrl: string): Promise<string> {
+    const unixCommand = `ffmpeg -i ${videoUrl} 2>&1 | grep "Duration" | sed -n 's/.*Duration: \\([0-9]\\{2\\}:[0-9]\\{2\\}:[0-9]\\{2\\}\\).*/{ "Duraion": "\\1" }/p'`;
+    const windowCommand = `for /f "tokens=2 delims= " %a in ('ffmpeg -i ${videoUrl} 2^>^&1 ^| findstr "Duration"') do @echo {"Duration": "%a"}`;
+
+    // Determine OS and run the appropriate command
+    const command = os.type() === 'Windows_NT' ? windowCommand : unixCommand;
+
+    try {
+        const { stdout } = await execPromise(command);
+        console.log(stdout.trim())
+        return stdout.trim();  // Return the output, trimmed to remove any extra whitespace
+    } catch (error) {
+        console.error('Error executing command:', error);
+        throw error;
+    }
 }
